817c80698a115f78b57ad6ace36dab124edcd83f
simranpatthak <suman.patthak1166@gmail.com>
2025-06-27 11:24:37 +0530

    update in string methods

--- javascript/var-let-const/Stringsmethods/main.js
+++ javascript/var-let-const/Stringsmethods/main.js
@@ -1,40 +1,58 @@
-// 1.
-// let text =`This is strings "method"`;
-// console.log(text)
+// javascript template litrels
+1.
+let text =`This is strings "method"`;
+console.log(text)
 
-// 2.
+// Backslash
+2.
+let text1="we are sp called \"viking\"from the north we are sp called \"viking\"from the north \v we are sp called \"viking\"from the northwe are sp called \"viking\"from the north"
+console.log(text1)
 
-// let text1="we are sp called \"viking\"from the north we are sp called \"viking\"from the north \v we are sp called \"viking\"from the northwe are sp called \"viking\"from the north"
-// console.log(text1)
+// javascript comparision method
+3.
+let x= "simran";
+let y=new String("simran");
+console.log(x==y)
 
-// 3.
-// let x= "simran";
-// let y=new String("simran");
+// javascript strict comparison
+let x1= "simran";
+let y1=new String("simran");
 
-// console.log(x==y)
+console.log(x1===y1)
 
-// let x1= "simran";
-// let y1=new String("simran");
+// jascript find length method
+5.
+let text2="simran pathak";
+let length=text2.length;
+console.log(length)
 
-// console.log(x1===y1)
+6.
+let text3="simran";
+let char=text3.charCodeAt(5)
+console.log(char)
 
 
-// 5.
-// let text2="simran pathak";
-// let length=text2.length;
-// console.log(length)
+// javscript charat methoda
+7.
+const name1="W3school";
+let letter=name1.at(2)
+console.log(letter)
 
-// 6.
-// let text3="simran";
-// let char=text3.charCodeAt(5)
-// console.log(char)
-
-// 7.
-// const name1="W3school";
-// let letter=name1.at(2)
-// console.log(letter)
-
+// javascript slice
 let text4="Apple ,banana, orange, grapes"
 let part=text4.slice(5)
 
 console.log(part)
+
+// Javascript Replace method
+let text5="Welcome to JS"
+let text41=text5.replace("JS","Javscript")
+console.log(text41)
+
+// Javascript ReplaceAll method
+
+let text6="Welcome to Javascript,let began Javascript"
+let text7=text6.replaceAll("Javascript", "javascript")
+console.log(text7)
+
+
--- javascript/var-let-const/main.js
+++ javascript/var-let-const/main.js
@@ -1,39 +1,30 @@
-// var a;
-// var a=10;
-// var a=20;
-// console.log(a)
+// javscript touppercase
+let text1="simran pathak";
+let text2=text1.toUpperCase()
+console.log(text2)
 
-// var b=10;
+// Javascript concat method
+let text0="hello"+ ""+"World";
+text="hello".concat("","world!")
+console.log(text)
 
-// {
-//     var b=20
-// }
-// console.log(b)
+// jascript padEnd method
+let number="5";
+let padded=number.padEnd(5,"x")
+console.log(padded)
 
+// javascript splite method string
+let text= "Simran Pathak";
+const myArr=text.split('');
 
-// let text1="simran pathak";
-// let text2=text1.toUpperCase()
-// console.log(text2)
+text='';
 
-// let text="hello"+ ""+"World";
-// text="hello".concat("","world!")
-// console.log(text)
+for(let i=0;i<myArr.length;i++){
+    text+=myArr[i]
+}
+console.log(myArr)
 
-// let number="5";
-// let padded=number.padEnd(5,"x")
-// console.log(padded)
-
-
-// let text= "Simran Pathak";
-// const myArr=text.split('');
-
-// text='';
-
-// for(let i=0;i<myArr.length;i++){
-//     text+=myArr[i]
-// }
-// console.log(myArr)
-
+// Reverse string 
 
 let str="simran"
 let rev=[...str].reverse().join('');
