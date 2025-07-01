a3fd325dd726af09ded90f734f853d25ea2bb99b
simranpatthak <suman.patthak1166@gmail.com>
2025-07-01 18:43:56 +0530

    update in js

---
+++ javascript/calculator/main.js
@@ -0,0 +1,20 @@
+const Display = document.getElementById("display");
+const Button = document.querySelector("button");
+
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculateResult(){
+    try {
+        const result=eval(document.getElementById("display").value);
+        document.getElementById("display").value=result;
+    } catch (error) {
+        document.getElementById("display").value="Error";
+        
+    }
+}
---
+++ javascript/functions/index.js
@@ -0,0 +1,13 @@
+function add(n1, n2){
+    return n1+n2;
+}
+
+let sum = add(12, 20);
+console.log(sum);
+
+const mul = (a, b) => {
+    console.log(a*b);
+}
+
+let multiply = mul(4, 5);
+console.log(multiply);
