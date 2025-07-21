7db1d4dceb9b22f63f7d0f323f9b2fdcf0588ef6
simranpatthak <suman.patthak1166@gmail.com>
2025-07-21 19:03:37 +0530

    update in made in function

--- javascript/Functionday2/main.js
+++ javascript/Functionday2/main.js
@@ -1,8 +1,8 @@
-// function multiply (a,b){
-//     return a*b
-// }
-// let multi=multiply(2,3)
-// console.log(multi)
+function multiply (a,b){
+    return a*b
+}
+let multi=multiply(2,3)
+console.log(multi)
 
 //  find even number using function
 function even(n){
@@ -36,9 +36,79 @@ console.log(max([2,3,4,5,6,]))
 
 
 // Double the number
-
 const num=[2,4,5,6,7]
 const double= num.map(num=>num*2);
 console.log(double)
+
+// Normal function
+function myfunction(name){
+    console.log("hello" + name);
+}
+myfunction("javascript")
+
+//  prefind argument agar koi agrument nahi pass ki gyi hai toh bhi yeh by default ek toh print hoga hi
+function function1(name="javascript"){
+    console.log("hello"+ name);
+}
+function1()
+
+
+//  Rest operator
+function myfunction(...num){
+    return num.map((a,b)=>a+b);
+}
+console.log(myfunction(1,2,3,4,5))
+
+// setTimeout
+
+setTimeout(function(){
+    console.log("hey i run in every 2 second");
+    
+    setTimeout(function(){
+    console.log("hey i run in every 3 second");
+},3000);
+setTimeout(function(){
+    console.log("hey i run in every 4 second");
+},4000);
+},2000);
+
+// this key word arrow function me kam nahi karta until and unless woh global nahi hai 
+// 	Inherits this from parent scope.
+
+// Pure function / immpure function 
+
+function add2(a,b){
+     return a+b;
+}
+
+const addition=add2(2,3)
+console.log(addition)
+
+// callback ek function k under pass kia jata hai 
+// ek task khtm hone k bad kam karta hai 
+// use to handle asynchronus  task (like api call ,timer)
+
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+
+function opreation (num1, num2, callback){
+    const result=callback(num1, num2);
+    console.log(result)
+}
+
+opreation(10,3, add)
+opreation (7,2,sub)
+
+
+
+
+
+
+
+
 
 
